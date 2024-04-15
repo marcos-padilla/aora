bedeb2a85226a67e2b4cf7179076815f0a543a9c
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-04-14 20:02:21 -0400

    feat(frontend): set up project

---
+++ app/_layout.jsx
@@ -0,0 +1,15 @@
+import { Slot, Stack } from 'expo-router'
+import { Text } from 'react-native'
+
+export default function RootLayout() {
+	return (
+		<Stack>
+			<Stack.Screen
+				name='index'
+				options={{
+					headerShown: false,
+				}}
+			/>
+		</Stack>
+	)
+}
